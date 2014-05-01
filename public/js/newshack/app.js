bf3658ce0f30343ffc5199cb22c88dd778bd9338
James Cryer <info@jamescryer.com>
2014-05-01 23:23:11 +0100

    Adding height restriction

--- public/js/newshack/app.js
+++ public/js/newshack/app.js
@@ -25,12 +25,22 @@ define([
     initialize: function() {
       $(function() {
         var story = $('.widget').data('story');
-        console.log('Loading story' + story);
+
         Store.get(story, function(data) {
           new Views.Header(data).render();
           new Views.Title(data.storyline).render();
           new Views.Topics(data.storyline.topics).render();
           new Views.Storyline(data).render();
+
+          $('.widget').each(function() {
+            var widget = $(this);
+            var header = $('.header', widget).height();
+            var title  = $('.title', widget).height();
+            var topics = $('.topics', widget).height();
+            var height = header + title + topics;
+            $('.content', widget).height((480 - height) + 'px');
+          });
+
         });
       });
 
