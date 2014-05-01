9be9cb0435b3ac9ea4b6224dba93b58481fccff9
James Cryer <info@jamescryer.com>
2014-05-01 12:09:03 +0100

    Adding backbone and jquery

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+require.config({
+  baseUrl: 'js/newshack',
+  paths: {
+    jquery: '/lib/jquery/dist/jquery',
+    underscore: '/lib/underscore/underscore',
+    backbone: '/lib/backbone/backbone'
+  }
+});
+
+require(['app'], function(App) {
+  App.initialize();
+});
---
+++ public/js/newshack/app.js
@@ -0,0 +1,10 @@
+define([
+  // These are path alias that we configured in our bootstrap
+  'jquery',     // lib/jquery/jquery
+  'underscore', // lib/underscore/underscore
+  'backbone'    // lib/backbone/backbone
+], function($, _, Backbone){
+  return {
+    initialize: function() {}
+  }
+});
